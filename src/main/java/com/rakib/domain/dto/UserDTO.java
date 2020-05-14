2a44863068329c08af32c5e0b1a95c825411bf63
rakib <jhadul.islam@itconquest.com>
2020-05-15 02:13:55 +0600

    fix Bug

--- src/main/java/com/rakib/domain/dto/UserDTO.java
+++ src/main/java/com/rakib/domain/dto/UserDTO.java
@@ -1,16 +1,26 @@
 package com.rakib.domain.dto;
 
-import org.springframework.stereotype.Component;
 
+import org.springframework.stereotype.Component;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.List;
+
 @Component
-public class UserDTO {
+public class UserDTO  {
+    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
+            "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])" +
+            "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|" +
+            "[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-" +
+            "\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    @NotNull
     private String userName;
     private String userPhone;
-    @Email
+    @Email(regexp = EMAIL_REGEX,message = "Email should be valid")
     private String userEmail;
+    @NotNull
     private String userPassword;
+    @NotNull
     private List<String> role;
 
     public String getUserName() {
@@ -52,4 +62,5 @@ public class UserDTO {
     public void setRole(List<String> role) {
         this.role = role;
     }
+
 }
